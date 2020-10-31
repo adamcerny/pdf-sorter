0fe338fa28ee752fbea757814a98f8005200fef6
Adam Cerny <adam.cerny@abelandcole.co.uk>
2020-10-31 15:16:57 +0000

    Add project files.

---
+++ pdf-sorter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pdf_sorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
